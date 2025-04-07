fbfe81bcc8140ba7678aa57671b6358f1c895df8
Kyle Sanderfer <kjs15@rice.edu>
2025-04-07 18:15:34 -0500

    Simple main logic for CLI testing

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from src.market_poker.engine import run_game
+from src.market_poker.engine import GameEngine
+from src.market_poker.player import Player
+
 
 if __name__ == "__main__":
-    run_game()
+    # Create players
+    players = []
+    done_players = False
+    while not done_players:
+        players.append(Player(str(input("Enter player name: "))))
+        is_ready = input("    Done submitting players? (y/n): ").strip().lower()
+        if is_ready == "y":
+            done_players = True
+
+    # Launch game
+    engine = GameEngine(players)
+    engine.run_game()
